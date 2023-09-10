0080d25cd7535e733055ab6eb88d26b055bba8bf
chris-macharia <christophermaina003@gmail.com>
2023-09-10 15:08:32 +0300

    Print out 3 messages to screen

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstStatement = 'C is fun';
+const secondStatement = 'Python is cool';
+const thirdStatement = 'JavaScript is amazing';
+
+console.log(firstStatement);
+console.log(secondStatement);
+console.log(thirdStatement);
